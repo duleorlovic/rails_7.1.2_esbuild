8d8c995a84bfe779c8f71cf02a4016e4b360f085
Dusan Orlovic <dule@hey.com>
2024-02-13 09:09:05 +0100

    Add mdb

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,5 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+import * as mdb from "../../vendor/mdb5-free-standard/js/mdb.umd.min.js"
+window.mdb = mdb;
